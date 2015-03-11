7449d68906e3e03124fcb444984359a2b0273a05
Geert van Horrik <geert@catenalogic.com>
2015-03-11 19:03:59 +0100

    Added notification service extensions

---
+++ src/Orc.Notifications/Services/Extensions/INotificationServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="INotificationServiceExtensions.cs" company="Wild Gums">
+//   Copyright (c) 2008 - 2015 Wild Gums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.Notifications
+{
+    using System.Threading.Tasks;
+    using Catel;
+
+    public static class INotificationServiceExtensions
+    {
+        public static async Task ShowNotification(this INotificationService notificationService, string title, string message)
+        {
+            Argument.IsNotNull(() => notificationService);
+
+            var notification = new Notification
+            {
+                Title = title,
+                Message = message
+            };
+
+            await notificationService.ShowNotification(notification);
+        }
+    }
+}
