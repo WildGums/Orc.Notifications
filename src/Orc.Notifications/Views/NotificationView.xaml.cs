5dd3438f8e54a970fc16dc16d5dd6f202438a613
Geert van Horrik <geert@catenalogic.com>
2015-03-17 16:22:57 +0100

    Prevent notification from being the active window

--- src/Orc.Notifications/Views/NotificationView.xaml.cs
+++ src/Orc.Notifications/Views/NotificationView.xaml.cs
@@ -7,9 +7,17 @@
 
 namespace Orc.Notifications
 {
-    using System.Drawing;
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using System.Windows;
+    using System.Windows.Input;
     using Catel.IoC;
     using Catel.Windows;
+    using Catel.Windows.Threading;
+    using Size = System.Drawing.Size;
 
     /// <summary>
     /// Interaction logic for NotificationView.xaml.
@@ -17,6 +25,44 @@ namespace Orc.Notifications
     public partial class NotificationView
     {
         private static readonly Size NotificationSize = new Size(Orc.Notifications.NotificationSize.Width, Orc.Notifications.NotificationSize.Height);
+
+        private enum HookType : int
+        {
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern uint GetCurrentThreadId();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(HookType hookType, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        private delegate int HookProc(int code, IntPtr wParam, IntPtr lParam);
+
+        private readonly HookProc _hookProc;
+        private IntPtr _hook;
+
+        private System.Windows.Window _mainWindow;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NotificationView"/> class.
@@ -38,14 +84,94 @@ namespace Orc.Notifications
         {
             InitializeComponent();
 
+            var threadId = GetCurrentThreadId();
+
+            // Note: very important to store this in a field to prevent CallbackOnCollectedDelegateException
+            // ReSharper disable once RedundantDelegateCreation
+            _hookProc = new HookProc(CbtCallbackFunction);
+            _hook = SetWindowsHookEx(HookType.WH_CBT, _hookProc, IntPtr.Zero, threadId);
+
             var dependencyResolver = this.GetDependencyResolver();
             var notificationPositionService = dependencyResolver.Resolve<INotificationPositionService>();
 
             var notificationLocation = notificationPositionService.GetLeftTopCorner(NotificationSize);
 
-            WindowStartupLocation = System.Windows.WindowStartupLocation.Manual;
+            WindowStartupLocation = WindowStartupLocation.Manual;
             Left = notificationLocation.X;
             Top = notificationLocation.Y;
+        }
+
+        protected override void OnLoaded(EventArgs e)
+        {
+            base.OnLoaded(e);
+
+            var application = Application.Current;
+            if (application != null)
+            {
+                _mainWindow = application.MainWindow;
+                if (_mainWindow != null)
+                {
+                    _mainWindow.Closing += OnMainWindowClosing;
+                }
+            }
+        }
+
+        protected override void OnUnloaded(EventArgs e)
+        {
+            if (_mainWindow != null)
+            {
+                _mainWindow.Closing -= OnMainWindowClosing;
+                _mainWindow = null;
+            }
+
+            base.OnUnloaded(e);
+        }
+
+        private void OnMainWindowClosing(object sender, CancelEventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            ActivatePreviousWindow();
+
+            var vm = ViewModel as NotificationViewModel;
+            if (vm != null)
+            {
+                var command = vm.Command;
+                if (command != null)
+                {
+                    command.Execute(null);
+                }
+            }
+
+            e.Handled = true;
+        }
+
+        private void ActivatePreviousWindow()
+        {
+            var windowHandles = Application.Current.Windows;
+            if (windowHandles.Count > 1)
+            {
+                var lastWindowHandle = windowHandles[windowHandles.Count - 2];
+                lastWindowHandle.Activate();
+            }
+        }
+
+        private int CbtCallbackFunction(int code, IntPtr wParam, IntPtr lParam)
+        {
+            Owner = null;
+
+            switch (code)
+            {
+                case 5: /* HCBT_ACTIVATE */
+                    UnhookWindowsHookEx(_hook);
+                    return 1; /* prevent windows from handling activate */
+            }
+
+            //return the value returned by CallNextHookEx
+            return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
         }
     }
 }
