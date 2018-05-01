47a3ff11e7e19c90588222fa41e72749baffedf7
Geert van Horrik <geert@catenalogic.com>
2018-05-01 11:32:58 +0200

    Migrate test project to new project structure

--- src/Orc.Notifications.Tests/PublicApiFacts.cs
+++ src/Orc.Notifications.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.Notifications.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_Notifications_HasNoBreakingChanges()
         {
             var assembly = typeof(NotificationService).Assembly;
