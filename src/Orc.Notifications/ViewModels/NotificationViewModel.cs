d5145a7fab7b9cc569675f916a85824d2774e9df
Geert van Horrik <geert@catenalogic.com>
2021-06-28 20:11:28 +0200

    Disable validation on notification viewmodels

--- src/Orc.Notifications/ViewModels/NotificationViewModel.cs
+++ src/Orc.Notifications/ViewModels/NotificationViewModel.cs
@@ -42,6 +42,9 @@ namespace Orc.Notifications
             PauseTimer = new Command(OnPauseTimerExecute);
             ResumeTimer = new Command(OnResumeTimerExecute);
             ClosePopup = new TaskCommand(OnClosePopupExecuteAsync);
+
+            // Validation makes no sense on notifications
+            AutomaticallyValidateOnPropertyChanged = false;
         }
 
         #region Properties
@@ -116,7 +119,8 @@ namespace Orc.Notifications
         private void OnDispatcherTimerTick(object sender, EventArgs e)
         {
 #pragma warning disable 4014
-            this.SaveAndCloseViewModelAsync();
+            // Cancel to make sure we don't enable validation
+            this.CancelAndCloseViewModelAsync();
 #pragma warning restore 4014
         }
 
