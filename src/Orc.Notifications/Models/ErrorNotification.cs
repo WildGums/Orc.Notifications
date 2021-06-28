bfff12eeb0745ebbae929d820c3285711e5aff8c
Geert van Horrik <geert@catenalogic.com>
2021-06-28 20:35:59 +0200

    Freeze default notification brushes to allow multithreaded usage

--- src/Orc.Notifications/Models/ErrorNotification.cs
+++ src/Orc.Notifications/Models/ErrorNotification.cs
@@ -5,13 +5,20 @@
 
     public class ErrorNotification : Notification
     {
+        private static readonly SolidColorBrush DefaultBackgroundBrush = new SolidColorBrush(Color.FromArgb(255, 242, 91, 67));
+
+        static ErrorNotification()
+        {
+            DefaultBackgroundBrush.Freeze();
+        }
+
         public ErrorNotification()
         {
             ShowTime = TimeSpan.FromSeconds(5);
             Priority = NotificationPriority.High;
             Level = NotificationLevel.Error;
             BorderBrush = Brushes.Red;
-            BackgroundBrush = new SolidColorBrush(Color.FromArgb(255, 242, 91, 67));
+            BackgroundBrush = DefaultBackgroundBrush;
         }
     }
 }
