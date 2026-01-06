c772dfaaa47311584bd6701c9e1760923d36f2ce
Geert van Horrik <geert@catenalogic.com>
2026-01-06 10:29:30 +0100

    Update

---
+++ src/Orc.Notifications.Tests/Helpers/ServiceCollectionHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Orc.Notifications.Tests
+{
+    using Catel;
+    using Microsoft.Extensions.DependencyInjection;
+    using Orc.Theming;
+
+    internal static class ServiceCollectionHelper
+    {
+        public static IServiceCollection CreateServiceCollection()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging();
+            serviceCollection.AddCatelCore();
+            serviceCollection.AddCatelMvvm();
+            serviceCollection.AddOrcNotifications();
+            serviceCollection.AddOrcTheming();
+
+            return serviceCollection;
+        }
+    }
+}
